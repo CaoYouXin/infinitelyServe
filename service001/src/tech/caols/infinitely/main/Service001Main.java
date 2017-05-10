f6707366879cdf5b0b62fdf292956c9ae99abe82
曹力升 <c_youxin@163.com>
2017-05-10 20:32:12 +0800

    服务001，测试。

---
+++ service001/src/tech/caols/infinitely/main/Service001Main.java
@@ -0,0 +1,39 @@
+package tech.caols.infinitely.main;
+
+import org.apache.http.protocol.*;
+import tech.caols.infinitely.config.ConfigUtil;
+import tech.caols.infinitely.config.ShutDownConfig;
+import tech.caols.infinitely.config.SimpleConfig;
+import tech.caols.infinitely.handlers.UserAddHandler;
+import tech.caols.infinitely.server.SimpleServer;
+import tech.caols.infinitely.server.Stopper;
+
+public class Service001Main {
+
+    public static void main(String[] args) {
+        ConfigUtil util = new ConfigUtil();
+
+        switch (args[0]) {
+            case "start":
+
+                final SimpleConfig config = util.getConfig(util.getRootFileName() + ".json", SimpleConfig.class);
+                SimpleServer simpleServer = new SimpleServer(config.getPort(), config.getDocRoot())
+                        .registerHandler("/user/add", new UserAddHandler());
+                simpleServer.start(() -> {
+                    System.out.println("service 001 started.");
+                    System.out.println(config);
+                });
+
+                break;
+            case "stop":
+
+                ShutDownConfig shutDownConfig = util.getConfig(util.getRootFileName() + ".log", ShutDownConfig.class);
+                new Stopper(shutDownConfig.getHostName(), shutDownConfig.getHostPort(), shutDownConfig.getToken()).call();
+
+                break;
+            default:
+                break;
+        }
+    }
+
+}
