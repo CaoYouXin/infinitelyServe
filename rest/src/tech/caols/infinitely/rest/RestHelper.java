a3225ea3ef0ac02b0c977ff55851d5fe615e9ac4
曹力升 <c_youxin@163.com>
2017-05-24 15:12:45 +0800

    add some log , but not working?!?

--- rest/src/tech/caols/infinitely/rest/RestHelper.java
+++ rest/src/tech/caols/infinitely/rest/RestHelper.java
@@ -1,11 +1,15 @@
 package tech.caols.infinitely.rest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import tech.caols.infinitely.server.SimpleServer;
 import tech.caols.infinitely.server.handlers.ProxyHandler;
 
 import java.lang.reflect.Method;
 
 public class RestHelper {
+
+    private static final Logger logger = LogManager.getLogger(RestHelper.class);
 
     private final SimpleServer simpleServer;
 
@@ -20,15 +24,18 @@ public class RestHelper {
             throw new RuntimeException("not a rest object.");
         }
 
+        logger.info("parsing @ " + object.getClass().getName());
         for (Method method : object.getClass().getMethods()) {
             RestAPI restAPI = method.getDeclaredAnnotation(RestAPI.class);
 
             if (restAPI != null) {
+                logger.info("serving @ " + restAPI.url());
                 this.simpleServer.registerHandler(restAPI.url(), new ProxyHandler(
                     new RestHandler(object, method, restAPI)
                 ));
             }
         }
+        logger.info("parsing finished @ " + object.getClass().getName());
 
         return this;
     }
